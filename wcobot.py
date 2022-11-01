f9120fd9caea2f4dced0a49ff627eb1347d7c221
DarioDaF <dario.fagotto@gmail.com>
2022-11-01 23:32:15 +0100

    Make links and save status

--- wcobot.py
+++ wcobot.py
@@ -20,12 +20,15 @@ Press Ctrl-C on the command line or send a signal to the process to stop the
 bot.
 """
 
-VERSION = '0.2.2'
+VERSION = '0.3.0'
+SAVE_FILE = './wcobot.json'
 
-import logging
+import logging, json, os
 
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, Job
+from telegram import Update, ParseMode
+from telegram.ext import Updater, CommandHandler, CallbackContext, Job, JobQueue
+#from telegram.utils.helpers import escape_markdown
+from html import escape
 
 from gen.wco import checkPage
 
@@ -44,26 +47,49 @@ def start(update: Update, context: CallbackContext) -> None:
 
 MAX_ENTRIES = 5
 
-class UserScrapeCtx:
-    def __init__(self):
-        self.latests: dict[str, str] = {}
-        self.jobs: dict[str, Job] = {}
+class JobDescr:
+    def __init__(self, chat_id, page: str, interval: int, latest: str = ''):
+        self.chat_id = chat_id
+        self.page = page
+        self.interval = interval
+        self.job: Job = None
+        self.latest = latest
+    def create(self, job_queue: JobQueue):
+        job_name = f'Scrape|{self.chat_id}|{self.page}'
+        self.job = job_queue.run_repeating(_alarm, interval=self.interval, context=self, name=job_name)
+        return self.job
+    def remove(self):
+        if self.job != None:
+            self.job.schedule_removal()
+            self.job = None
+    def is_valid(self):
+        return self.job != None
+    def to_dict(self):
+        return {
+            'chat_id': self.chat_id,
+            'page': self.page,
+            'interval': self.interval,
+            'latest': self.latest,
+        }
 
 class ScrapeJobs:
     def __init__(self):
-        self.jobs: dict[int, UserScrapeCtx] = {}
-    def add(self, context: CallbackContext, chat_id, page, interval):
-        job_name = f'Scrape|{chat_id}|{page}'
+        self.jobs: dict[int, dict[str, JobDescr]] = {}
+    def add(self, job_queue: JobQueue, chat_id, page, interval, latest=''):
         if chat_id not in self.jobs:
-            self.jobs[chat_id] = UserScrapeCtx()
-        job = context.job_queue.run_repeating(alarm, interval, context=(chat_id, page, self.jobs[chat_id].latests), name=job_name)
-        self.jobs[chat_id].jobs[page] = job
-        return job
+            self.jobs[chat_id] = {}
+        if page in self.jobs[chat_id]:
+            self.jobs[chat_id][page].remove()
+            #del self.jobs[chat_id][page] # Gets overwritten
+        jd = JobDescr(chat_id, page, interval, latest)
+        self.jobs[chat_id][page] = jd
+        jd.create(job_queue)
+        return jd
     def remove(self, chat_id, page = ''):
         if chat_id not in self.jobs:
             return 0
-        user_jobs = self.jobs[chat_id].jobs
-        target_jobs = []
+        user_jobs = self.jobs[chat_id]
+        target_jobs: list[JobDescr] = []
         if page != '':
             if page in user_jobs:
                 target_jobs.append(user_jobs[page])
@@ -72,23 +98,27 @@ class ScrapeJobs:
             target_jobs.extend(user_jobs.values())
             user_jobs.clear()
         for job in target_jobs:
-            job.schedule_removal()
+            job.remove()
         return len(target_jobs)
 scrape_jobs = ScrapeJobs()
 
-def alarm(context: CallbackContext):
+def _alarm(context: CallbackContext):
     """Send the alarm message."""
-    chat_id, page, latests = context.job.context
+    job_descr: JobDescr = context.job.context
     resp = ''
     i = 0
-    for title, href in checkPage(page, latests):
+
+    latest_list = { job_descr.page: job_descr.latest }
+    for title, href in checkPage(job_descr.page, latest_list):
         if i < MAX_ENTRIES:
-            resp += f'{title}\n'
+            resp += f'<a href="{escape(href)}">{escape(title)}</a>\n'
         i += 1
+    job_descr.latest = latest_list[job_descr.page]
+
     if i > MAX_ENTRIES:
-        resp += f'... [other {i - MAX_ENTRIES} elements]'
+        resp += f'<i>... [other {i - MAX_ENTRIES} elements]</i>'
     if resp != '':
-        context.bot.send_message(chat_id, text=resp)
+        context.bot.send_message(job_descr.chat_id, text=resp, parse_mode=ParseMode.HTML)
 
 def set_scrape(update: Update, context: CallbackContext) -> None:
     """Add a job to the queue."""
@@ -102,14 +132,14 @@ def set_scrape(update: Update, context: CallbackContext) -> None:
             return
 
         job_removed = scrape_jobs.remove(chat_id, page) > 0
-        job = scrape_jobs.add(context, chat_id, page, interval)
+        jd = scrape_jobs.add(context.job_queue, chat_id, page, interval)
 
         text = 'Timer successfully set!'
         if job_removed:
             text += ' Old one was removed.'
         update.message.reply_text(text)
 
-        job.run(context.dispatcher) # Force first run
+        jd.job.run(context.dispatcher) # Force first run
 
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /scrape <seconds> <page>')
@@ -143,6 +173,14 @@ def main():
     dispatcher.add_handler(CommandHandler("scrape", set_scrape))
     dispatcher.add_handler(CommandHandler("unset", unset))
 
+    # Load Jobs
+    if os.path.isfile(SAVE_FILE):
+        logger.info('Loading saved jobs')
+        with open(SAVE_FILE, 'r') as fp:
+            all_jobs_data = json.load(fp)
+        for job in all_jobs_data:
+            scrape_jobs.add(updater.job_queue, job['chat_id'], job['page'], job['interval'], job['latest'])
+
     # Start the Bot
     updater.start_polling()
 
@@ -151,6 +189,14 @@ def main():
     # non-blocking and will stop the bot gracefully.
     updater.idle()
 
+    # Save jobs
+    logger.info('Saving jobs')
+    all_jobs_data = []
+    for user_jobs in scrape_jobs.jobs.values():
+        for job in user_jobs.values():
+            all_jobs_data.append(job.to_dict())
+    with open(SAVE_FILE, 'w') as fp:
+        json.dump(all_jobs_data, fp)
 
 if __name__ == '__main__':
     main()
